0b8be97bf34be1100feda6771c746d879804da5a
yormuhamad <153153430+yormuhamad@users.noreply.github.com>
2023-12-07 08:41:45 +0500

    Add files via upload
    Файлҳои нав сохта шудааст. Ин лоиҳаи қисмати аввал аст. Давом дорад

---
+++ Новая папка/index.js
@@ -0,0 +1,179 @@
+// Sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+
+//Message
+const messageNotification = document.querySelector('#message-notification');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+//===========Theme====================
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+const fontSize = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalete = document.querySelector('.choose-color span');
+
+
+
+// ===============Sidebar=================
+
+// remove active class from all menu items
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notification'){
+            document.querySelector('.notification-popup').style.display = 'none';
+             } else{
+                document.querySelector('.notification-popup').style.display = 'block';
+                document.querySelector('#notification .notification-count').style.display = 'block';
+             }
+
+    })
+})
+
+
+//================Messages====================
+// searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+   
+    message.forEach(chat => {
+
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex';
+        } else{
+            chat.style.display = 'none';
+        }
+    })
+}
+
+//===========Messages====================
+// searches chats
+messageSearch.addEventListener('keyup', searchMessage);
+
+
+// hihihihih messages card when messages menu item is clicked
+messageNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messageNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+//===========theme/display customization
+const openThememodal = () => {
+    themeModal.style.display = 'grid';
+    
+    
+}
+
+//close modal
+const closeThemeModal = (e)  => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+//close
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThememodal);
+
+
+
+
+//===========FONTS===================
+
+//remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+    fontSize.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSize.forEach(size => {
+
+
+
+size.addEventListener('click', () => {
+    removeSizeSelector();
+    let fontSize;
+    size.classList.toggle('active');
+
+    if(size.classList.contains('font size-1')){
+        fontSize = '10px';
+        root.style.setProperty('----sticky-top-left', '5.4rem');
+        root.style.setProperty('----sticky-top-right', '5.4rem');
+    } else if(size.classList.contains('font size-2')){
+        fontSize = '13px';
+        root.style.setProperty('----sticky-top-left', '5.4rem');
+        root.style.setProperty('----sticky-top-right', '-7rem');
+    } else if(size.classList.contains('font size-3')){
+        fontSize = '16px';
+        root.style.setProperty('----sticky-top-left', '-2rem');
+        root.style.setProperty('----sticky-top-right', '-17rem');
+    } else if(size.classList.contains('font size-4')){
+        fontSize = '19px';
+        root.style.setProperty('----sticky-top-left', '-5rem');
+        root.style.setProperty('----sticky-top-right', '-25rem');
+    } else if(size.classList.contains('font size-5')){
+        fontSize = '22px';
+        root.style.setProperty('----sticky-top-left', '-10rem');
+        root.style.setProperty('----sticky-top-right', '-35rem');
+
+    }
+
+     // change font size of the root html element
+     document.querySelector('html').style.fontSize = fontSize;
+})
+   
+})
+
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalete.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+//change primary colors
+colorPalete.forEach(color => {
+    color.addEventListener('click', () => {
+        let primary;
+        if(color.classList.contains('color-1')){
+            primaryhue = 252;
+           // remove active class from colors
+            changeActiveColorClass();
+        }else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        }else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        }else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        }else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        
+        color.classList.add('active');  
+
+        root.style.setProperty('--primary-color-hue', primaryhue);
+    })
+})
+
+
+
+
